8de22ab9256c1f93fd2442bb1b2927cccffcfdd8
Robert Aslan <robert.aslan44@gmail.com>
2025-02-07 13:47:55 -0500

    Player Movement

--- ufo-game/Scripts/player.gd
+++ ufo-game/Scripts/player.gd
@@ -1,17 +1,11 @@
 extends CharacterBody2D
 
+@export var speed = 400
 
-const SPEED = 300.0
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-
-func _physics_process(delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	get_input()
 	move_and_slide()

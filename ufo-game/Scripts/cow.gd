f4a62517d0d06751e86e9667e3e5405d1edbdc7b
Matt-Lamo <mattlejeune.lamogau@gmail.com>
2025-02-19 13:12:40 -0500

    Updated Cow.gd

--- ufo-game/Scripts/cow.gd
+++ ufo-game/Scripts/cow.gd
@@ -2,32 +2,39 @@ extends Area2D
 
 @onready var animatedSprite = $AnimatedSprite2D
 @onready var collisionShape = $CollisionShape2D
-@export var velocity = 10
-
+@export var speed = 100
+var isAbducted = false
+var collected = false
+var count = 40 
+var playerPosition: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	monitoring = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func abduct(body) -> void:
-	if position != body.position:
-		if body.position.x > position.x:
-			position.x += velocity
+	if collected:
+		#PUT IN SIGNAL FOR UI UPDATING AND SCOREBOARD HERE
+		queue_free()
+	if isAbducted:
+		if scale != (Vector2(0,0)):
+			count -= 1
+			scale = Vector2(scale.x-.1, scale.y-.1)
 		else:
-			position.y -= velocity
-		if body.position.y > position.y:
-			position.y += velocity
-		else:
-			position.y -= velocity
+			collected = true
 	else:
-		collisionShape.disabled = true
-		visible = false
+		count = 40
+		
+		
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Beam":
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "Beam":
 		print("Entered Beam body") 
-		abduct(body)
+		playerPosition = area.position
+		isAbducted = true
+		
+func _on_area_exited(area: Area2D) -> void:
+	if area.name == "Beam":
+		print("Left Beam body") 
+		isAbducted = false

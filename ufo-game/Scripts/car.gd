4101a26c636b3f3c2291be14b58342a08759e263
Matt-Lamo <mattlejeune.lamogau@gmail.com>
2025-02-21 13:23:34 -0500

    Car.gd Updated, added pathing
    Cars follow path2d tracks, drive around. Objects are properly abductable now.

--- ufo-game/Scripts/car.gd
+++ ufo-game/Scripts/car.gd
@@ -3,17 +3,24 @@ extends Area2D
 
 @onready var animatedSprite = $AnimatedSprite2D
 @onready var collisionShape = $CollisionShape2D
-@export var speed = 100
+@export var speed = 0.1
 var isAbducted = false
 var collected = false
 var countMax = 50.0
 var count = countMax
 var playerPosition: Vector2
+var position_last_frame := Vector2()
+var cardinal_direction = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	monitoring = true
 
+func detect_direction():
+	var motion = position - position_last_frame
 
+	if motion.length() > 0.0001:
+		
+		cardinal_direction = int(4.0 * (motion.rotated(PI / 4.0).angle() + PI) / TAU)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if collected:
@@ -30,7 +37,17 @@ func _process(delta: float) -> void:
 	else:
 		count = countMax
 		scale = Vector2(1,1)
-		
+	detect_direction()
+	match cardinal_direction:
+		0: #West
+			animatedSprite.play("Left")
+		1: #North
+			animatedSprite.play("Up")
+		2: #East
+			animatedSprite.play("Right")
+		3: #South
+			animatedSprite.play("Down")
+			
 		
 
 func _on_area_entered(area: Area2D) -> void:
--- ufo-game/Scripts/player.gd
+++ ufo-game/Scripts/player.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 225
+@onready var animatedSprite = $AnimatedSprite2D
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	match input_direction:
+		"left":
+			pass
+		"right":
+			pass
+		"up":
+			animatedSprite.animation.play("Up")
+		"down":
+			animatedSprite.animation.play("Down")
+			
 	velocity = input_direction * speed
 
 func _physics_process(delta):

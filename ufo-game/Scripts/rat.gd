83b396894fd9857ffb693af0b3b24216e36f1474
Matt-Lamo <mattlejeune.lamogau@gmail.com>
2025-03-14 13:06:10 -0400

    Rat Scene added
    Rat Scene added
    Tilemaps updated
    Trash Scene Updated

--- ufo-game/Scripts/rat.gd
+++ ufo-game/Scripts/rat.gd
@@ -2,18 +2,51 @@ extends Area2D
 
 @onready var animatedSprite = $AnimatedSprite2D
 @onready var collisionShape = $CollisionShape2D
-@export var speed = 100
+@export var speed = 0.5
 var isAbducted = false
 var collected = false
 var countMax = 50.0
 var count = countMax
-# Called when the node enters the scene tree for the first time.
+var velocity = Vector2(0,0)
+var moveCounter = true
+var rng = RandomNumberGenerator.new()
+@onready var newPosition = Vector2(position.x+randf_range(-8,8),position.y+randf_range(-8,8))
+var randx = 0
+var randy = 0
+func wait(seconds:float) -> void:
+	await get_tree().create_timer(seconds).timeout
+	
+	
 func _ready() -> void:
 	monitoring = true
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	animatedSprite.play("Idle")
+	rng.randomize()
+	
+func update_animation() -> void:
+	if randx > 0:
+		animatedSprite.flip_h = true
+	else:
+		animatedSprite.flip_h = false
+	
+func random_movement() -> void:
+	animatedSprite.play("Running")
+	if position.distance_to(newPosition)<1.0:
+		
+		randx = randf_range(-4,4)
+		randy = randf_range(-4,4)
+		print(randx)
+		print(randy)
+		newPosition = Vector2(position.x+randx,position.y+randy)
+		
+	position = position.move_toward(newPosition, speed)
+	
+	
+func _physics_process(delta: float) -> void:
+	random_movement()
+	
+	
 func _process(delta: float) -> void:
+	update_animation()
 	if collected:
 		#PUT IN SIGNAL FOR UI UPDATING AND SCOREBOARD HERE
 		Global.collectedTable["Rat"] += 1
@@ -30,15 +63,19 @@ func _process(delta: float) -> void:
 	else:
 		count = countMax
 		scale = Vector2(1,1)
-		
-		
+
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "Beam":
 		print("Entered Beam body") 
 		isAbducted = true
 		
+		
 func _on_area_exited(area: Area2D) -> void:
 	if area.name == "Beam":
 		print("Left Beam body") 
 		isAbducted = false
+
+
+func _on_timer_timeout() -> void:
+	moveCounter = true

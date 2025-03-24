0859507431d976e1afb9d6ad3fcfb6bc96947a23
flameshower <122758100+flameshower@users.noreply.github.com>
2025-03-24 13:52:14 -0400

    Soldier improvements
    Created scripts, etc.

---
+++ ufo-game/Scenes/SoldierArea2D.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _shoot_at_player() -> void:
+	# Algorithm:
+	# if player is in radius of n pixels:
+	# 	play shooting animation
+	# 	fire projectile at player
+	# 	if player is hit:
+	#		playerHealth -= 50
+	#	else:
+	#		projectile disappears if it collides with anything
+	pass
+
+func _on_soldiers_view_area_entered(area: Area2D) -> void:
+	if area.name == "player" :
+		_shoot_at_player()
+		pass
